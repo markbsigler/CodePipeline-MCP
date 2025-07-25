ed0e395c0005596104f95c88e13f71d11dd0e352
Mark B. Sigler <markbsigler@gmail.com>
2025-07-25 17:16:45 -0400

    chore: consolidate ESLint config to .eslintrc.js, suppress unused Request type warning, add type=module

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,3 +1,14 @@
+import type { Request as ExpressRequest } from 'express';
+
+ 
+export type Request = ExpressRequest & {
+  user?: {
+    id: string;
+    username: string;
+    roles?: string[];
+    [key: string]: unknown;
+  };
+};
 import { JwtPayload } from "jsonwebtoken";
 
 declare module "express-serve-static-core" {
